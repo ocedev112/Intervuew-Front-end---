e48c6e59b09145fd0adc59b1d38448b46c6cf616
unknown <olewuenyie@gmail.com>
2026-03-16 19:15:49 +0100

    app changes

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `/api`,
+  baseURL: import.meta.env.DEV
+    ? `/api`
+    : import.meta.env.VITE_BACKEND_API_ENDPOINT,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
